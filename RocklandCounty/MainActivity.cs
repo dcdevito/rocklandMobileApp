104195ff8e1428d5229f6a2ec9a391554048710d
Dominick DeVito <ddevito@gmail.com>
2016-09-05 14:01:22 -0400

    continued on login screen, added fragment for sign up

--- RocklandCounty/MainActivity.cs
+++ RocklandCounty/MainActivity.cs
@@ -11,7 +11,8 @@ namespace RocklandCounty
     [Activity(Label = "RocklandCounty", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        
+
+        private Button mBtnSignUp;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -22,8 +23,21 @@ namespace RocklandCounty
 
             // Get our button from the layout resource,
             // and attach an event to it
-          
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction(); //this pulls up the dialog from the activity
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+                
+            };
+
+                      
         }
+
+        
     }
 }
 
